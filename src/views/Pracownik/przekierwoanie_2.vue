3c6232b977a2740e5ce27d5b1b14c8b82f69d059
kmieciklukasz <kmiecik39@gmail.com>
2022-02-17 23:15:31 +0100

    1

--- src/views/Pracownik/przekierwoanie_2.vue
+++ src/views/Pracownik/przekierwoanie_2.vue
@@ -13,7 +13,7 @@
         Dziękujemy za zarejstrowanie na naszej stronie.
       </h6>
       <h7>Możesz się pomyślnie zalogować.</h7>
-      <meta http-equiv="Refresh" content="3;url=/Login" />
+      <meta http-equiv="Refresh" content="3;url=/login" />
       <b-card-text> </b-card-text>
     </b-card>
   </div>
